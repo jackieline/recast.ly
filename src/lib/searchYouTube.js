2ef49afe6461eacad5d7adbab5e12528ac705e8f
Hack Reactor Student <student@hackreactor.com>
2017-10-25 20:04:49 -0700

    searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,32 @@
+var options = {
+  key: window.YOUTUBE_API_KEY,
+  query: 'penguins',
+  max: 5
+};
+
+
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      key: options.key,
+      maxResults: options.max,
+      q: options.query,
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: true
+    },
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(data) {
+      console.error(data);
+    }
+    // dataType: dataType,
+
+  });
 };
 
 window.searchYouTube = searchYouTube;
